f2a9a5b55b4dbd29c80b5bc075b9a629fb50ca1b
Fyrid <jkeonde@gmail.com>
2024-04-09 23:32:38 -0400

    YESS IT WORKED try this now

---
+++ actionBuild.bat
@@ -0,0 +1,20 @@
+haxelib install flixel-addons 3.0.2 --quiet
+haxelib install flixel-demos 2.9.0 --quiet
+haxelib install flixel-templates 2.6.6 --quiet
+haxelib install flixel-tools 1.5.1 --quiet
+haxelib install flixel-ui 2.5.0 --quiet
+haxelib install flixel 5.3.0 --quiet
+haxelib install hxcpp 4.2.1 --quiet
+haxelib install flxanimate 3.0.4 --quiet
+haxelib install hscript 2.5.0 --quiet
+haxelib install lime-samples 7.0.0 --quiet
+haxelib install lime 8.0.2 --quiet
+haxelib install openfl 9.2.2 --quiet
+haxelib install haxeui-core 1.6.0 --quiet
+haxelib install haxeui-flixel 1.6.0 --quiet
+haxelib install newgrounds --quiet
+haxelib install hxCodec 3.0.2 --quiet
+haxelib git polymod https://github.com/larsiusprime/polymod --quiet
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc --quiet
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit --quiet
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate --quiet
