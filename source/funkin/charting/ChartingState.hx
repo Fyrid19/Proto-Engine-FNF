a841374b67190c009eb5b2cd7aac25caaec94222
Fyrid <jkeonde@gmail.com>
2024-04-13 16:41:54 -0400

    erm action build being dumb??

--- source/funkin/charting/ChartingState.hx
+++ source/funkin/charting/ChartingState.hx
@@ -1,11 +1,8 @@
 package funkin.charting;
 
-import utils.Conductor;
-import utils.CoolUtil;
 import funkin.objects.Section.SwagSection;
 import funkin.objects.Song.SwagSong;
-import flixel.FlxG;
-import flixel.FlxSprite;
+
 import flixel.addons.display.FlxGridOverlay;
 import flixel.addons.ui.FlxInputText;
 import flixel.addons.ui.FlxUI9SliceSprite;
@@ -16,27 +13,25 @@ import flixel.addons.ui.FlxUIInputText;
 import flixel.addons.ui.FlxUINumericStepper;
 import flixel.addons.ui.FlxUITabMenu;
 import flixel.addons.ui.FlxUITooltip.FlxUITooltipStyle;
-import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.group.FlxGroup;
+
 import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.sound.FlxSound;
-import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.ui.FlxSpriteButton;
-import flixel.util.FlxColor;
+
 import haxe.Json;
+
 import lime.utils.Assets;
+
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import openfl.media.Sound;
 import openfl.net.FileReference;
 import openfl.utils.ByteArray;
+
 import funkin.objects.notes.Note;
 import funkin.objects.notes.NoteSplash;
-import funkin.objects.*;
-import funkin.states.PlayState;
-import funkin.states.LoadingState;
 
 using StringTools;
 
