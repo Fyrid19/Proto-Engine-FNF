165e15799171592bcb2455472475483dc59dcaba
Fyrid <jkeonde@gmail.com>
2024-04-16 23:50:44 -0400

    aughhghh ill fix errors later

--- source/backend/framerate/FPSCounter.hx
+++ source/backend/framerate/FPSCounter.hx
@@ -16,11 +16,12 @@ class FPSCounter extends Sprite {
 		fpsCounter = new TextField();
 		fpsLabel = new TextField();
 
+		fpsCounter.text = "";
 		fpsLabel.text = "FPS:";
-		
     }
 
 	public override function __enterFrame(delta:Int) {
-		var curFPS:Float = Stats.currentFps();
+		var curFPS:Float = Stats.currentFps(); // i do not know if this actually works because vshaxe is being stupid
+		fpsCounter.text = curFPS;
 	}
 }
