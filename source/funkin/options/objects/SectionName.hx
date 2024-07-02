a637a5399313d02c6a65d0a0c86cc43f379dd0a6
Fyrid <jkeonde@gmail.com>
2024-07-02 13:55:37 -0400

    iont think the font workd

--- source/funkin/options/objects/SectionName.hx
+++ source/funkin/options/objects/SectionName.hx
@@ -7,7 +7,7 @@ class SectionName extends FlxSpriteGroup {
     public function new(x:Float, y:Float, name:String) {
         super(x, y);
         text = new FlxText(x, y, FlxG.width, name, 64);
-        text.setFormat(Paths.font('muff.ttf'), 64, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        text.setFormat(Paths.font('vcr.ttf'), 64, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
     }
 
     override function update(elapsed:Float) {
