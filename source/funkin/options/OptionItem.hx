40cae7fcc47e30f652a93854ee2cca98b69aa872
Fyrid <jkeonde@gmail.com>
2024-08-03 17:36:13 -0400

    option thing

--- source/funkin/options/OptionItem.hx
+++ source/funkin/options/OptionItem.hx
@@ -6,14 +6,19 @@ class OptionItem extends FlxSpriteGroup {
     var nameText:FlxText;
     var valueText:FlxText;
 
-    public function new(x:Float, y:Float, text:String) {
+    var name:String;
+    var value:String;
+
+    public function new(x:Float, y:Float, name:String, startingValue:Dynamic) {
         super(x, y);
 
-        nameText = new FlxText(x, y, FlxG.width, '', 32);
+        nameText = new FlxText(x, y, FlxG.width * 0.8, '', 32);
         nameText.setFormat(Paths.font('vcr.ttf'), 32, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(nameText);
 
-        valueText = new FlxText(x, y, FlxG.width, '', 32);
+        valueText = new FlxText(x, y, FlxG.width * 0.8, '', 32);
         valueText.setFormat(Paths.font('vcr.ttf'), 32, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(valueText);
     }
 
     override function update(elapsed:Float) {
