55e662335c2dc71cebb25c0d104c87c894a6417b
Fyrid <jkeonde@gmail.com>
2024-08-31 15:47:48 -0400

    command thing or something

---
+++ cmd/Main.hx
@@ -0,0 +1,40 @@
+package;
+
+typedef HaxelibData = {
+    name:String,
+    version:String,
+    git:String
+}
+
+class Main {
+    public static var haxelibs = sys.io.File.getContent('cmd/data/haxelibs.json');
+    public static var haxelibsParsed:Dynamic;
+    public static var libraries:Array<HaxelibData>;
+    public static function main() {
+        var args:Array<String> = ['', '', ''];
+        haxelibsParsed = haxe.Json.parse(haxelibs);
+        libraries = haxelibsParsed.libraries;
+
+        for (lib in libraries) {
+            var install:String = 'install';
+            var version:String = null;
+            var git:String = null;
+
+            if (lib.git.length > 1) {
+                install = 'git';
+                git = lib.git;
+            }
+
+            if (lib.version.length > 1) {
+                version = lib.version;
+            }
+
+            if (git != null) args = [install, lib.name, git];
+            else if (version != null) args = [install, lib.name, version];
+            else args = [install, lib.name];
+
+            // Sys.println(args);
+            Sys.command('haxelib', args);
+        }
+    }
+}
